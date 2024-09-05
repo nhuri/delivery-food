6090ea6c37d30e4cab5af428a0eaffb0e81b208f
Netanel Huri <natihuri1992@gmail.com>
2024-09-05 15:33:18 +0300

    fixing card

--- client/src/restraunt/RestrauntItem.jsx
+++ client/src/restraunt/RestrauntItem.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const RestrauntItem = ({
   id,
@@ -11,15 +13,24 @@ const RestrauntItem = ({
   statistics,
 }) => {
   return (
-    <Link to="#" className="card">
-      <div>
-        <img src={logo} alt={name} />
-        <h2>{name}</h2>
-        <h3>{address}</h3>
-        {/* <Link to={`/${menu}`}>menu</Link> */}
-        <h3>{statistics}</h3>
-      </div>
-    </Link>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={logo} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{address}</Card.Text>
+        {/* <Card.Text>{statistics}</Card.Text> */}
+        <Button variant="primary">menu</Button>
+      </Card.Body>
+    </Card>
+    // <Link to="#" className="card">
+    //   <div>
+    //     <img src={logo} alt={name} />
+    //     <h2>{name}</h2>
+    //     <h3>{address}</h3>
+    //     {/* <Link to={`/${menu}`}>menu</Link> */}
+    //     <h3>{statistics}</h3>
+    //   </div>
+    // </Link>
   );
 };
 
