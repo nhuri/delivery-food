5906f44059fbee513b04156539aa71e93164bec5
AviadLevi <aviad889@gmail.com>
2024-09-05 10:31:38 +0300

    connect backend

---
+++ server/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+// const usersRouter = require('./routes/user.Routes')
+const AppError = require('./utils/AppError')
+const globalErrorHandler = require('./utils/errorHandler')
+const app = express()
+app.use(cookieParser())
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(cors(
+    {
+        credentials: true,
+        origin: ["http://127.0.0.1:5500"]
+    }
+))
+app.use(cookieParser())
+
+
+// app.use('/api/products' , productsRouter)
+// app.use('/api/users' , usersRouter)
+
+
+
+app.all('*', (req, res, next) =>{
+    next(new AppError(404,'The requested route is not exist'))
+})
+
+app.use(globalErrorHandler)
+module.exports = app
